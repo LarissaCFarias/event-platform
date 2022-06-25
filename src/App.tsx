fd7a173eac5a462a2848e0ce5af7d983462a9995
Constantino <larissaconstantino@yahoo.com.br>
2022-06-24 22:39:29 -0300

    aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { Header } from "./components/Header";
-import { Lesson } from "./components/Lesson";
-import { Player } from "./components/Player";
-import { Sidebar } from "./components/Sidebar";
-import { client } from "./lib/apollo"
-import {Event} from "././pages/Event"
-
-const GET_LESSONS_QUERY = gql`
-  query{ 
-    lessons{
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id:string;
-  title:string;
-}
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom";
+import { client } from "./lib/apollo";
+import { Router } from "./router";
 
 function App() {
-  // useEffect(() => {
-  //   client.query({query: GET_LESSONS_QUERY}).then(response => {
-  //     console.log(response.data);
-  //   })
-  // }, [])
-  const {data} = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY);
-  console.log(data);
-
   return (
-    <div>
-    <Event />
-    </div>
+       <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
   )
 }
 
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,12 +1,22 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
+import { PlayerClasses } from "../components/Player";
 import { Sidebar } from "../components/Sidebar";
 
+
 export function Event(){
+  const{slug} = useParams<{slug: string}>()
   return(
-    <div>
+    <div className="flex flex-col min-h-screen">
     <Header />
-    <Sidebar />
-    <h6>PAREI NO SEGUNDO VIDEO EM 29:58</h6>
+    <main className="flex flex-1">
+      {slug ?  
+        <PlayerClasses lessonSlug={slug}/> 
+        : <div className="flex-1"/>
+      }
+     
+      <Sidebar />
+    </main>
     </div>
   )
 }
